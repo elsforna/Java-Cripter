2299a2415380a2a993c436e762d28229f057e3b0
Nicolò Vescera <slayercandy@icloud.com>
2016-01-15 16:28:46 +0100

    Aggiunti commenti e inzio a creare classe Alfabeto

--- src/alfabetonascosto/AlfabetoNascosto.java
+++ src/alfabetonascosto/AlfabetoNascosto.java
@@ -18,16 +18,15 @@ public class AlfabetoNascosto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //dichiarazione di stringhe per input,output e confronto
         String inserito;
         String out = "";
+        String confronto;
         
+        //dichiarazione di iteratori i,j
         int i,j;
-        int dimS;
-        //Dichiarazione Input da tastiera
         
-        //Archivio dell"alfabeto latino
-        //ArrayList alf1 = new ArrayList();
-        
+        //Archivio dell"alfabeto latin
         //array di stringhe
         String[] alf1 = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","w","x","y","x"," "};
         
@@ -35,14 +34,16 @@ public class AlfabetoNascosto {
         //ArrayList alf2 = new ArrayList(); 
         String[] alf2 = {"x","-","_","_-","/-","/_","-_","-_-","/.",".","-./","_.-","..","__","0","_/","0.","/0","$","€","_..","<>","/._","_0.","x.","^"};
 
-//Inserimento Stringa confronto
+        //Inserimento Stringa inserito
         inserito = inputS("Inserisci la stringa da convertire:");
-        String confronto;
         
+        //ciclo per caratteri di inserito
         for(i = 0;i<inserito.length();i++){
+            //ciclo per elementi di alf1
             for(j=0;j<alf1.length;j++){
-                //potrebbe dare ebola
+                //confronta il carattere di inserito in posizione i con ogni posizione di alf1
                 if(inserito.charAt(i) == (char)alf1[j].charAt(0)){
+                    //aggiunge alla stringa out la posizione j di alf2
                     out = out+alf2[j];
                     break;
                 }
