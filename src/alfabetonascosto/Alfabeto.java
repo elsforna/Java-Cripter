eaa5cd04d325febb7673f4d775500632922b7d7c
Nicolò Vescera <slayercandy@icloud.com>
2016-01-15 16:39:04 +0100

    Creata ed implemetnata classe Alfabeto

--- src/alfabetonascosto/Alfabeto.java
+++ src/alfabetonascosto/Alfabeto.java
@@ -11,8 +11,35 @@ package alfabetonascosto;
  */
 
 public class Alfabeto {
-  
-
-  
+        //Archivio dell"alfabeto latin -- array di stringhe
+        private final String[] alf1 = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","w","x","y","x"," "};
+        //Archivo lettere alfabeto nuovo -- array di stringhe
+        private final String[] alf2 = {"x","-","_","_-","/-","/_","-_","-_-","/.",".","-./","_.-","..","__","0","_/","0.","/0","$","€","_..","<>","/._","_0.","x.","^"};
+        
+        public Alfabeto(String inserito){
+            codifica(inserito);
+        }
+        public  void codifica(String inserito){
+            //dichiarazione di stringhe per output e confronto
+            String out = "";
+            String confronto;
+            
+            //dichiarazione di iteratori i,j
+            int i,j;
+            
+            //ciclo per caratteri di inserito
+            for(i = 0;i<inserito.length();i++){
+            //ciclo per elementi di alf1
+                for(j=0;j<this.alf1.length;j++){
+                    //confronta il carattere di inserito in posizione i con ogni posizione di alf1
+                    if(inserito.charAt(i) == (char)this.alf1[j].charAt(0)){
+                        //aggiunge alla stringa out la posizione j di alf2
+                        out = out+this.alf2[j];
+                        break;
+                    }
+                }
+            }  
+            System.out.println(out);
+        }
   
 }
--- src/alfabetonascosto/AlfabetoNascosto.java
+++ src/alfabetonascosto/AlfabetoNascosto.java
@@ -18,40 +18,25 @@ public class AlfabetoNascosto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //dichiarazione di stringhe per input,output e confronto
+        
         String inserito;
-        String out = "";
-        String confronto;
         
-        //dichiarazione di iteratori i,j
-        int i,j;
         
-        //Archivio dell"alfabeto latin
-        //array di stringhe
-        String[] alf1 = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","w","x","y","x"," "};
         
-        //Archivo lettere alfabeto nuovo
+        
+        
+        
+        
+        
         //ArrayList alf2 = new ArrayList(); 
-        String[] alf2 = {"x","-","_","_-","/-","/_","-_","-_-","/.",".","-./","_.-","..","__","0","_/","0.","/0","$","€","_..","<>","/._","_0.","x.","^"};
 
         //Inserimento Stringa inserito
         inserito = inputS("Inserisci la stringa da convertire:");
         
-        //ciclo per caratteri di inserito
-        for(i = 0;i<inserito.length();i++){
-            //ciclo per elementi di alf1
-            for(j=0;j<alf1.length;j++){
-                //confronta il carattere di inserito in posizione i con ogni posizione di alf1
-                if(inserito.charAt(i) == (char)alf1[j].charAt(0)){
-                    //aggiunge alla stringa out la posizione j di alf2
-                    out = out+alf2[j];
-                    break;
-                }
-           
-            }
-        }
+        Alfabeto cod = new Alfabeto(inserito);
         
-        System.out.println(out);
+        
+        
     
     }
     
