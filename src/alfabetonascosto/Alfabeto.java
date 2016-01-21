a3d12129e0d3c7e51c172ed730074029359a5e09
Nicolò Vescera <slayercandy@icloud.com>
2016-01-21 20:57:22 +0100

    Aggiunto spazio tra le parole

--- src/alfabetonascosto/Alfabeto.java
+++ src/alfabetonascosto/Alfabeto.java
@@ -14,13 +14,12 @@ public class Alfabeto {
         //Archivio dell"alfabeto latin -- array di stringhe
         static final String[] alf1 = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "};
         //Archivo lettere alfabeto nuovo -- array di stringhe
-        static final String[] alf2 = {"x","-","_","_-","/-","/_","-_","-_-","/.",".","-./","_.-","..","__","0","_/","0.","/0","$","€","_..","<","<>","/._","_0.","x.","##"};
+        static final String[] alf2 = {"x","-","_","_-","/-","/_","-_","-_-","/.",".","-./","_.-","..","__","0","_/","0.","/0","$","€","_..","<","<>","/._","_0.","x."," "};
         
         
         public static void codifica(String inserito){
             //dichiarazione di stringhe per output e confronto
             String out = "";
-            String confronto;
             
             //dichiarazione di iteratori i,j
             int i,j;
@@ -51,32 +50,21 @@ public class Alfabeto {
             String sub;
             //ciclo per caratteri di inserito
             while(inserito.length() != 0){
-            //ciclo per elementi di alf1
-               // if(inserito.length() != 2){
-                //System.out.println("Strigna: "+inserito+"LEN: "+inserito.length()+"I: "+i+"\n");
+
                 index = inserito.indexOf('#');
-                //System.out.println(index);
                 sub = inserito.substring(0,index);
-                //System.out.println(sub);
-                
-                    for(j=0;j<alf1.length;j++){
-                        //confronta il carattere di inserito in posizione i con ogni posizione di alf1
-                        if(sub.equals(alf2[j])){
-                            //aggiunge alla stringa out la posizione j di alf2
-                            //out = out+alf2[j]+"    #    ";
-                            out = out+alf1[j];
-                            break;
-                        }
+
+                for(j=0;j<alf1.length;j++){
+                    if(sub.equals(alf2[j])){
+                        out = out+alf1[j];
+                        break;
                     }
-                    //inserito = inserito.replace(alf2[i],alf1[i]);
-                    inserito = inserito.substring(index+1,inserito.length());
-                    //System.out.println(inserito);
-                //} 
-              //System.out.println("Strigna: "+inserito+"LEN: "+inserito.length()+"I: "+i+"\n");
-            
+                }
+                
+                inserito = inserito.substring(index+1,inserito.length());
             }
+            
             System.out.println(out);
-  
     }
         
 }
--- src/alfabetonascosto/AlfabetoNascosto.java
+++ src/alfabetonascosto/AlfabetoNascosto.java
@@ -10,7 +10,7 @@ public class AlfabetoNascosto {
         
         //Documentazione sul Software
         inputS("Codice che effettua la conversione di una stringa da Alfabeto Latino in Codice SCripter inserita in Input"
-                + "\n - A/a = X\n - B/b = -\n - C/c = _\n - D/d = _-\n - E/e = /-\n - F/f = /_\n - G/g = __\n - H/h = -_-\n - I/i = /.\n - J/j = .\n - K/k = -./\n - L/l = _.-\n - M/m = ..\n - N/n = __\n - O/o = 0\n - P/p = _/\n - Q/q = 0.\n - R/r = /0\n - S/s = $\n - T/t = €\n - U/u = _..\n - W/w = <>\n - X/x = /._\n - Y/y = _0.\n - Z/z = X.\n - Space Between Letter = #\n - Space Between Words = ##\n \"Digitare il Carrage Return:\"   ");
+                + "\n - A/a = X\n - B/b = -\n - C/c = _\n - D/d = _-\n - E/e = /-\n - F/f = /_\n - G/g = __\n - H/h = -_-\n - I/i = /.\n - J/j = .\n - K/k = -./\n - L/l = _.-\n - M/m = ..\n - N/n = __\n - O/o = 0\n - P/p = _/\n - Q/q = 0.\n - R/r = /0\n - S/s = $\n - T/t = €\n - U/u = _..\n - W/w = <>\n - X/x = /._\n - Y/y = _0.\n - Z/z = X.\n - Space Between Letter = #\n \"Digitare il Carrage Return:\"   ");
         //Inserimento Stringa inserito
         inserito = inputS("Inserisci la stringa da convertire:");
         
